9973b2dfcc3f6154f079380e68fe4bbe3390ac48
roland-sipos <sipos.roland.cb@gmail.com>
2020-10-12 13:34:16 +0200

    remove hardcoded answer port and explain what it is

--- scripts/send-cmd.py
+++ scripts/send-cmd.py
@@ -2,9 +2,10 @@ import argparse
 import requests
 import json
 
-parser = argparse.ArgumentParser(description='POST command object from file to commanded applications.')
+parser = argparse.ArgumentParser(description='POST command object from file to commanded endpoint.')
 parser.add_argument('--host', type=str, default='localhost', help='target host/endpoint')
 parser.add_argument('-p', '--port', type=int, default=12345, help='target port')
+parser.add_argument('-a', '--answer-port', type=int, default=12333, help='answer to service listening on this port')
 parser.add_argument('-r', '--route', type=str, default='command',help='target route on endpoint')
 parser.add_argument('-f', '--file', type=str, help='file that contains command to be posted')
 
@@ -12,7 +13,7 @@ args = parser.parse_args()
 
 url = 'http://'+args.host+':'+str(args.port)+'/'+args.route
 print('Target url: ' + url)
-headers = {'content-type': 'application/json', 'x-answer-port': 12333}
+headers = {'content-type': 'application/json', 'x-answer-port': args.answer_port}
 
 cmd = {}
 with open(args.file) as f:
