5a072dc5c2da798e427fa87e4dbe44a0f76b5bcf
Eric Flumerfelt <eflumerf@fnal.gov>
2025-02-10 09:16:48 -0600

    Include logging/Logging.hpp whenever ERS issues are declared, including note on why (TLOG() << ERSIssue only works when Logging.hpp included first)

--- include/restcmd/Issues.hpp
+++ include/restcmd/Issues.hpp
@@ -10,6 +10,7 @@
 
 
 #include "ers/Issue.hpp"
+#include <logging/Logging.hpp> // NOTE: if ISSUES ARE DECLARED BEFORE include logging/Logging.hpp, TLOG_DEBUG<<issue wont work.
 #include <string>
 
 namespace dunedaq {
